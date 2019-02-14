1958d6f98c06f6b2e923f0852690f5b47125026b
unknown <kyleteeter12@gmail.com>
2019-02-13 23:04:28 -0600

    Post method funtioning

--- api/server.js
+++ api/server.js
@@ -1,26 +1,27 @@
 const express = require('express');
-
-const notes = require('../helper/helper.js');
+const cors = require('cors');
+const db = require('../helper/helper.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', async (req, res) => {
   res.status(200).json({ api: 'up' });
 });
 
 server.get('/notes', async (req, res) => {
-  const rows = await notes.getAll();
+  const notes = await db.getAll();
 
-  res.status(200).json(rows);
+  res.status(200).json(notes);
 });
 
 server.post('/notes', async (req, res) => {
     const noteData = req.body;
    console.log(noteData)
-    if (noteData.title && noteData.genre) {
-      const ids = await notes.insert(noteData);
+    if (noteData.title && noteData.text) {
+      const ids = await db.insert(noteData);
       res.status(201).json(ids);
     } else {
       res.status(422).json({error: 'missing important information'})
--- helper/helper.js
+++ helper/helper.js
@@ -1,9 +1,14 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
+    insert,
     getAll,
 };
 
 function getAll() {
     return db('notes');
   }
+
+  function insert(note) {
+    return db('notes').insert(note);
+}
